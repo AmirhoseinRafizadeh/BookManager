6af7e4567054c8cea5fa637e0fc4253891d686d9
amirhosein rafizadeh <amirhosein.rafizadeh.dev@mail.com>
2025-07-22 22:11:29 +0330

    completed

--- BookManager/api/urls.py
+++ BookManager/api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('books/', views.BookListCreate.as_view(), name='book_create'),
-    path('books/<int:id>', views.BookDetail.as_view(), name='book_detail'),
+    path('books/<int:pk>', views.BookDetail.as_view(), name='book_detail'),
 ]
